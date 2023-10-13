3f5099bfb8b2e19a806cfb11b978e1866f4299c5
Illia Ponomarov <chippaxyucac@gmail.com>
2023-10-13 16:46:53 +0200

    init

--- src/app/componets/app.component.ts
+++ src/app/componets/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '../html/app.component.html',
+  styleUrls: ['../css/app.component.css']
 })
 export class AppComponent {
-  title = 'tbm-front';
+  title = 'tbm-ront';
+
+  
+
 }
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../componets/app.component';
+import { HeaderComponent } from '../componets/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
